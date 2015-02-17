9947f0349bb8aa9193d4dd86c6f3bce1ce720cf9
Malte <Malte@Malte>
2015-02-17 11:12:33 +0100

    Temperature Storage

--- TempatureProject/src/server/TemperatureStorage.java
+++ TempatureProject/src/server/TemperatureStorage.java
@@ -1,13 +1,9 @@
 package server;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,21 +12,22 @@ import java.util.List;
 public class TemperatureStorage {
 	
 	
-	public static boolean storeTemperature(char b) {
+	public static boolean storeTemperature(byte[] b) {
 		// TODO
 		/*
 		 * 1. Maybe convert bytes?
 		 * 2. Save data to file.
 		 * 3. Return true if successful, false otherwise
 		 */
+		TemperatureConverter.temperatureToChar(b);
 		try{
-		File file = new File("Temperature.txt");
-		file.createNewFile();
-		FileWriter writer = new FileWriter(file);
-		writer.write(b + ",");
-		writer.flush();
-		writer.close();
-		return true;
+			File file = new File("Temperature.txt");
+			file.createNewFile();
+			FileWriter writer = new FileWriter(file);
+			writer.write(b + ",");
+			writer.flush();
+			writer.close();
+			return true;
 		} catch (IOException e) {
 			e.getMessage();
 		}
@@ -46,15 +43,15 @@ public class TemperatureStorage {
 		 * 3. Return bytes read from disk
 		 */
 		try {
-		FileReader fr = new FileReader("Temperature.txt");
-		List<Character> a = new ArrayList<Character>();
-		
-		fr.read((CharBuffer) a);
-		for (char c : a) {
-			System.out.println(c);
-		}
-		fr.close();
-		return a;
+			FileReader fr = new FileReader("Temperature.txt");
+			List<Character> a = new ArrayList<Character>();
+			
+			fr.read((CharBuffer) a);
+			for (char c : a) {
+				System.out.println(c);
+			}
+			fr.close();
+			return a;
 		} catch(IOException e) {
 			e.getMessage();
 		}
