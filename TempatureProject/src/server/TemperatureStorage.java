28762ae49464881de23592942594f40fb0ccafef
Malte <Malte@Malte>
2015-02-17 11:07:53 +0100

    TemperatureStorage lavet om

--- TempatureProject/src/server/TemperatureStorage.java
+++ TempatureProject/src/server/TemperatureStorage.java
@@ -1,56 +1,64 @@
 package server;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.CharBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TemperatureStorage {
-	public static boolean storeTemperature(byte[] b) {
+	
+	
+	public static boolean storeTemperature(char b) {
 		// TODO
 		/*
 		 * 1. Maybe convert bytes?
 		 * 2. Save data to file.
 		 * 3. Return true if successful, false otherwise
 		 */
-		try {
-			FileOutputStream saveFile = new FileOutputStream("Temperatur.sav");
-			ObjectOutputStream save = new ObjectOutputStream(saveFile);
-			save.writeObject(b);
-			save.close();
+		try{
+		File file = new File("Temperature.txt");
+		file.createNewFile();
+		FileWriter writer = new FileWriter(file);
+		writer.write(b + ",");
+		writer.flush();
+		writer.close();
+		return true;
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			e.getMessage();
 		}
-		
 		
 		return false;
 	}
 	
-	public static byte[] readTemperature() {
-		byte[] b = {0};
+	public static List<Character> readTemperature() {
 		// TODO
 		/*
 		 * 1. Read file from disk
 		 * 2. If not given as bytes, convert back to bytes
 		 * 3. Return bytes read from disk
 		 */
-		FileInputStream saveFile;
-		ObjectInputStream restore;
-		
 		try {
-			saveFile = new FileInputStream("saveFile.sav");
-			restore = new ObjectInputStream(saveFile);
-			
-			b = (byte[]) restore.readObject();
-			
-			restore.close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		} catch (ClassNotFoundException e) {
-			System.out.println(e.getMessage());
+		FileReader fr = new FileReader("Temperature.txt");
+		List<Character> a = new ArrayList<Character>();
+		
+		fr.read((CharBuffer) a);
+		for (char c : a) {
+			System.out.println(c);
 		}
-		return b;
+		fr.close();
+		return a;
+		} catch(IOException e) {
+			e.getMessage();
+		}
+		return null;
 	}
+		
 }
