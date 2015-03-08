382acd30687b173bcf35c78daa12e27b52e1336c
Malte <Malte@Malte-pc>
2015-03-08 16:05:33 +0100

    TCleintRMI begyndt på

--- TempatureProject/src/client/TClientRMI.java
+++ TempatureProject/src/client/TClientRMI.java
@@ -1,11 +1,23 @@
 package client;
 
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import client.TClient;
 
-
-public interface client extends Remote {
-	<T> T executeTask(Task<T> t) throws RemoteException
-}
 public class TClientRMI {
-
+	public static void main (String args[]) {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		try {
+			String name = "Client";
+			Registry registry = LocateRegistry.getRegistry(args[0]);
+			TClient client = (TClient) registry.lookup(name);
+			System.out.println();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
